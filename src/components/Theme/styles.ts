a77c6bc8be24e5a84f7eb42c0ba3ac7183161ce4
Amós Silas <silastj@hotmail.com>
2022-09-19 23:47:08 -0300

    add body step

--- src/components/Theme/styles.ts
+++ src/components/Theme/styles.ts
@@ -5,11 +5,28 @@ export const Area = styled.section``;
 export const Steps = styled.div`
   display:flex;
   min-height: 100vh;
+
+
+  @media (max-width:768px){
+  flex-direction:column;
+  gap:20px;
+}
 `;
 export const Sidebar = styled.div`
   width:30%;
+
+@media (max-width:768px){
+  width:100%;
+}
 `;
 export const Page = styled.div`
   border-left:1px solid;
   width: 70%;
+
+  @media (max-width:768px){
+  width:100%;
+  border-top:1px solid;
+  border-left:0;
+}
 `;
+
--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,11 +1,27 @@
 import * as C from './styles'
 import Theme from '../../components/Theme/index'
+import { Link } from 'react-router-dom'
 
 const FormStep1 = () => {
+
   return (
     <Theme>
       <C.Container>
-        <h2>Step 01</h2>
+        <p>Passo 1/3</p>
+        <h1>Vamos começar com seu nome</h1>
+        <p>Preencha o campo abaixo com seu nome completo.</p>
+
+        <hr />
+
+        <label htmlFor="">
+          Seu nome completo
+          <input 
+            type="text"
+            autoFocus
+           />
+        </label>
+
+        <Link to="/step2">Próximo</Link>
       </C.Container>
     </Theme>
   )
